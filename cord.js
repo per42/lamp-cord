2c1ae9d31c120ee9602f4d9b85b8419b3e597011
Per Rosengren <per@stunning.se>
2013-02-01 14:37:40 +0100

    Bulb is SVG image. Content on example page. Jerk from scroll. Adjusted params

--- cord.js
+++ cord.js
@@ -1,8 +1,8 @@
 var LampCord = function(settings) {
 	var self = this;
 	self.settings = settings;
-	self.r = Raphael("holder", 900, 800);
-	self.el_progress = 0.5;
+	self.r = Raphael("holder", self.settings.paper.width, self.settings.paper.height);
+	self.el_progress = 0.0;
 	self.locations = [];
 	self.velocities = [];
 	for (var i=0; i<self.settings.guide.length; ++i) {
@@ -15,30 +15,49 @@ var LampCord = function(settings) {
 	    var shade = self.r.path();
 	    var x = i/(self.n_shades-1);
 	    var c = (120 + 100 * Math.sin(x * Math.PI / 2)).toFixed();
-	    var w = 2 + 8 * (1 - x);
+	    var w = self.settings.cord_width * (0.2 + 0.8 * (1 - x));
 	    shade.attr({
 	        'stroke-width': w,
 	        'stroke': "rgb(" + c + "," + c + "," + c + ")"
 	    });
 	    self.shades.push(shade);
 	}
-	self.bulb = self.r.rect(-50, -100, 100, 200, 10);
 	self.el = self.r.path();
 	self.el.attr({
 	    'stroke-width': "2px",
 //	    'stroke-linecap': "round",
 	    'stroke': '#ffa'
 	});
+//	self.bulb = self.r.rect(-50, -100, 100, 200, 10);
+	self.bulb = self.r.image("Ktip.svg", -200, -200, 400, 400);
 	self.run_loop = setInterval(function() {
 		self.move_cord();
 		self.draw_cord();
 	}, 0);
+	self.scroll_last = null;
+	window.onscroll = function() {
+		self.jerk_cord();
+	};
+};
+
+LampCord.prototype.jerk_cord = function() {
+	var self = this;
+	var speed = self.settings.movement.speed;
+	var scroll_now = window.pageYOffset;
+	if (self.scroll_last !== null) {
+		var scroll_diff = scroll_now - self.scroll_last;
+		for (var i=1; i<self.settings.guide.length; ++i) {
+			self.velocities[i][1] += scroll_diff * self.locations[i][1] * speed * self.settings.movement.jerk;
+		}
+	}
+	self.scroll_last = scroll_now;
+	self.el_progress = window.pageYOffset / (document.height -  window.innerHeight);
 };
 
 LampCord.prototype.move_cord = function() {
 	var self = this;
 	var speed = self.settings.movement.speed;
-	for (var i=0; i<self.settings.guide.length; ++i) {
+	for (var i=1; i<self.settings.guide.length; ++i) {
 		for (var j=0; j<2; ++j) {
 			self.velocities[i][j] += (self.settings.guide[i][j] - self.locations[i][j]) * speed;
 			self.velocities[i][j] += (Math.random()-0.5) * speed * self.settings.movement.randomness;
@@ -46,7 +65,6 @@ LampCord.prototype.move_cord = function() {
 			self.locations[i][j] += self.velocities[i][j];
 		}
 	}
-	self.el_progress = window.pageYOffset / (document.height -  window.innerHeight);
 };
 
 LampCord.prototype.draw_cord = function() {
@@ -65,7 +83,7 @@ LampCord.prototype.draw_cord = function() {
 	self.bulb.attr('transform', 
 			"T" + self.locations[last][0] + "," + self.locations[last][1] + 
 			"R" + angle.toFixed(1) + 
-			"t0,-100");
+			"t0,-195");
 	self.bulb.attr('fill', (self.el_progress == 1.0)? "#ffa": "none");
     self.el.attr('path', self.shades[0].getSubpath(0, 
     		self.el_progress * self.shades[0].getTotalLength()));
@@ -97,18 +115,39 @@ LampCord.prototype.get_path = function(p) {
 
 window.onload = function() {
 	var g_lamp_cord = new LampCord({
+		paper: {
+			width: 500,
+			height: 2800
+		},
 		guide: [
-		        [400, 100],
-    			[500, 200],
-				[400, 300],
-				[500, 400],
-				[450, 500],
-				[450, 550]
+		        [150, -10],
+		        [150, 100],
+    			[200, 200],
+				[100, 300],
+				[200, 400],
+				[100, 500],
+				[200, 600],
+				[100, 700],
+				[200, 800],
+				[100, 900],
+				[200, 1000],
+				[100, 1100],
+				[200, 1200],
+				[100, 1300],
+				[200, 1400],
+				[100, 1500],
+				[200, 1600],
+				[100, 1700],
+				[200, 1800],
+				[150, 1900],
+				[150, 1950]
 		        ],
 		movement: {
-			speed: 0.001,
-			damping: 1.0,
-			randomness: 100.0
-		}
+			speed: 0.002,
+			damping: 2.0,
+			randomness: 40.0,
+			jerk: 0.004
+		},
+		cord_width: 20
 	});
 };
